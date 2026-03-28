974ed402b8eee87bb74d388d4cf349533e334675
MuhammadUmer126 <mad.abdullahshahab47@gmail.com>
2026-03-28 15:50:46 +0500

    Added Table Logic

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 console.log("Helooooooooooooo");
+
+console.log("2 * 1 = 2");
+console.log("2 * 2 = 4");
+console.log("2 * 3 = 6");
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`2 * ${i} = ${2 * i}`);
+}
+console.log('========================================================================');
+for (let i = 10; i > 0; i--) {
+    console.log(`2 * ${i} = ${2 * i}`);
+}
